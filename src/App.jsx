697f42fbaae5295e119bb365cfa6997d64e38d53
MuhammadIqbalk <iqbalkresna.12@gmail.com>
2024-12-05 22:44:34 +0700

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import "./App.css";
+import "./index.css";
 import Note from "./components/note.jsx";
 
 function App() {
   return (
-    <div>
-      <Note title="Note 1" description="React note" />
-      <Note title="Note 2" description="Js note" />
-    </div>
+    <>
+      <div className="container mx-auto px-4 bg-black text-4xl font-bold text-blue-600">
+        <div>
+          <Note title="Note 1" description="React note" />
+          <Note title="Note 2" description="Js note" />
+        </div>
+      </div>
+    </>
   );
 }
 
